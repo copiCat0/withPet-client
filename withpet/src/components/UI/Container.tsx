2f00d4d2381b4ff8f3b0c1fd2789fe1eae027fcf
chaehaeun <chae_3210@naver.com>
2023-03-17 00:13:45 +0900

    [💄] justify 프롭스 추가

--- withpet/src/components/UI/Container.tsx
+++ withpet/src/components/UI/Container.tsx
@@ -2,12 +2,17 @@ import React, { ReactNode } from 'react'
 
 type ContainerProps = {
   bg: string | number
+  justify: string
   children: ReactNode
 }
 
-const Container: React.FC<ContainerProps> = ({ bg, children }) => {
+const Container: React.FC<ContainerProps> = ({ bg, children, justify }) => {
   return (
-    <main className={`h-screen mx-auto max-w-scr p-4 ${bg}`}>{children}</main>
+    <main
+      className={`h-screen mx-auto max-w-scr p-4 flex ${justify} flex-col ${bg}`}
+    >
+      {children}
+    </main>
   )
 }
 
