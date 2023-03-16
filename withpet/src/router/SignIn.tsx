afba1899e479ceb5a32b80b8244a9d9930115758
chaehaeun <chae_3210@naver.com>
2023-03-16 23:55:00 +0900

    [💄] sign in 페이지 html css #5

---
+++ withpet/src/router/SignIn.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+import SignInHead from 'assets/Logo/signInLogo.webp'
+import Container from 'components/UI/Container'
+import SignInInput from 'components/SignIn/SignInInput'
+import LinkButton from 'components/UI/LinkButton'
+import SignInLink from 'components/SignIn/SignInLink'
+
+const SignIn = () => {
+  const [idValue, setIdValue] = useState('')
+  const [pwValue, setPwValue] = useState('')
+
+  const getIdVal = (value: string): void => {
+    setIdValue(value)
+  }
+  const getPwVal = (value: string): void => {
+    setPwValue(value)
+  }
+
+  return (
+    <Container bg={'bg-primary-100'} justify={'justify-center'}>
+      <img src={SignInHead} alt="로그인" className="block mx-auto" />
+      <form>
+        <fieldset>
+          <legend className="sr-only">로그인 입력 폼</legend>
+          <div className={'flex flex-col gap-4 mt-5'}>
+            <SignInInput
+              type={'text'}
+              id={'userID'}
+              label={'이메일'}
+              inputValHandler={getIdVal}
+            />
+            <SignInInput
+              type={'password'}
+              id={'userPW'}
+              label={'비밀번호'}
+              inputValHandler={getPwVal}
+            />
+          </div>
+        </fieldset>
+      </form>
+      <LinkButton type={'button'} page={'/home'} text={'로그인'} />
+
+      <div className="mt-6">
+        <SignInLink to={'/'} text={'회원가입'} />
+        <SignInLink to={'/'} text={'아이디/비밀번호 찾기'} />
+      </div>
+    </Container>
+  )
+}
+
+export default SignIn
