8d1409090ccf743355de4df09f092a2366dff99b
sungeunnn <fronteun@naver.com>
2023-03-17 00:22:49 +0900

    [❇️] petInfo form에서 입력한 값 객체로 받아오기 #35

--- withpet/src/components/PetInfo/PetInfo.tsx
+++ withpet/src/components/PetInfo/PetInfo.tsx
@@ -1,56 +1,147 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from 'assets/Logo/petinfoLogo.webp'
-import PetInfoButton from 'components/PetInfo/PetInfoButton'
 import PetInfoInput from 'components/PetInfo/PetInfoInput'
 import 'components/App/App.css'
 
+import PetInfoRadioGroup from './PetInfoRadioGroup'
+import PetInfoRadioBtn from './PetInfoRadioBtn'
+import PetInfoRegister from './PetInfoRegister'
+
 const PetInfo = () => {
+  const [inputValues, setInputValues] = useState({
+    petImg: '',
+    petType: '',
+    petName: '',
+    petBirth: '',
+  })
+  const { petImg, petType, petName, petBirth } = inputValues
+
+  const [checkValues, setCheckValues] = useState({
+    petGender: 'male',
+    petNeuter: 'yes',
+  })
+
+  const [submit, setSubmit] = useState({ ...inputValues, ...checkValues })
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { name, value },
+    } = e
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    })
+  }
+
+  const onClick = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { name, value },
+    } = e
+    setCheckValues({
+      ...checkValues,
+      [name]: value,
+    })
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const petInfo = { ...inputValues, ...checkValues }
+    setSubmit(petInfo)
+
+    return submit
+  }
+
   return (
-    <div role="petInfogroup" className="flex flex-col items-center">
-      <h2>
-        <img src={Logo} alt="Pet Information Logo"></img>
-      </h2>
-      <form className="flex flex-col" aria-label="Pet Information">
-        <input type="file" accept="img/*" />
+    <section className="flex flex-col items-center justify-center bg-primary-100 max-w-scr h-screen">
+      <div role="petInfogroup">
+        <h2>
+          <img src={Logo} alt="Pet Information Logo"></img>
+        </h2>
+        <form
+          className="flex flex-col"
+          aria-label="Pet Information"
+          onSubmit={onSubmit}
+        >
+          <PetInfoInput
+            id="petImg"
+            name="petImg"
+            type="file"
+            accept="img/*"
+            onChange={onChange}
+            value={petImg}
+            className=""
+          />
 
-        <PetInfoInput id="petList" type="text" list="petList">
-          type
-        </PetInfoInput>
-        <datalist id="petList">
-          <option value="강아지" />
-          <option value="고양이" />
-          <option value="쥐" />
-          <option value="새" />
-          <option value="파충류" />
-        </datalist>
+          <PetInfoInput
+            id="petType"
+            name="petType"
+            type="text"
+            list="petList"
+            onChange={onChange}
+            value={petType}
+          >
+            type
+          </PetInfoInput>
+          <datalist id="petList">
+            <option value="강아지" />
+            <option value="고양이" />
+            <option value="쥐" />
+            <option value="새" />
+            <option value="파충류" />
+          </datalist>
 
-        <PetInfoInput id="petName" type="text">
-          이름
-        </PetInfoInput>
+          <PetInfoInput
+            id="petName"
+            name="petName"
+            type="text"
+            onChange={onChange}
+            value={petName}
+          >
+            이름
+          </PetInfoInput>
 
-        <PetInfoInput id="petBirth" type="date">
-          생년월일
-        </PetInfoInput>
+          <PetInfoInput
+            id="petBirth"
+            name="petBirth"
+            type="date"
+            onChange={onChange}
+            value={petBirth}
+          >
+            생년월일
+          </PetInfoInput>
 
-        <PetInfoInput id="petGender" type="hidden">
-          성별
-        </PetInfoInput>
-        <div role="petGenderGroup">
-          <PetInfoButton id="male">남</PetInfoButton>
-          <PetInfoButton id="female">여</PetInfoButton>
-        </div>
+          <PetInfoRadioGroup label="성별">
+            <PetInfoRadioBtn
+              defaultChecked
+              name="petGender"
+              value="male"
+              onClick={onClick}
+            >
+              남
+            </PetInfoRadioBtn>
+            <PetInfoRadioBtn name="petGender" value="female" onClick={onClick}>
+              여
+            </PetInfoRadioBtn>
+          </PetInfoRadioGroup>
 
-        <PetInfoInput id="petNeuter" type="hidden">
-          중성화여부
-        </PetInfoInput>
-        <div role="petNeuterGroup">
-          <PetInfoButton id="yes">예</PetInfoButton>
-          <PetInfoButton id="no">아니오</PetInfoButton>
-        </div>
+          <PetInfoRadioGroup label="중성화">
+            <PetInfoRadioBtn
+              defaultChecked
+              name="petNeuter"
+              value="yes"
+              onClick={onClick}
+            >
+              네
+            </PetInfoRadioBtn>
+            <PetInfoRadioBtn name="petNeuter" value="no" onClick={onClick}>
+              아니오
+            </PetInfoRadioBtn>
+          </PetInfoRadioGroup>
 
-        <input type="submit" value="등록하기" />
-      </form>
-    </div>
+          <PetInfoRegister id="submit" type="submit" value="등록하기" />
+        </form>
+      </div>
+    </section>
   )
 }
 
