2054d8ca741553c072a6bce5f4ab98dd374be6cf
Butter on Bread <qorbaxk97@gmail.com>
2023-03-16 16:29:07 +0900

    [:sparkles:] 사진첨부시 미리보기 기능  #24

--- withpet/src/components/Diary/AttachedPicture.tsx
+++ withpet/src/components/Diary/AttachedPicture.tsx
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SwiperPicture from './SwiperPicture'
 
-const AttachedPicture = () => {
+const AttachedPicture: React.FC = () => {
+  const [images, setImages] = useState<string[]>([])
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files!
+    if (!files[0]) return
+    if (images.length + files.length > 4) {
+      return alert('최대 4개 사진만 첨부할 수 있습니다.')
+    }
+    const readAndPreview = (file: any) => {
+      if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+        const reader = new FileReader()
+        reader.onload = () =>
+          setImages(prev => [...prev, reader.result as string])
+        reader.readAsDataURL(file)
+      }
+    }
+    if (files) {
+      [].forEach.call(files, readAndPreview)
+    }
+    console.log('함수 안', images)
+  }
+
+  console.log('함수 바깥', images)
+
   return (
-    <div className="w-full h-10 bg-Gray-100">
-      <label htmlFor="attach-img" >
-        <span className="flex w-full h-full justify-center items-center cursor-pointer">+ 사진첨부</span>
-        <input
-          id="attach-img"
-          type="file"
-          accept="image/*"
-          className="hidden"
-        />
-      </label>
-    </div>
+    <>
+      <form className="w-full h-10 bg-Gray-100" encType="multipart/form-data">
+        <label htmlFor="attach-img">
+          <span className="flex w-full h-full justify-center items-center cursor-pointer">
+            + 사진첨부
+          </span>
+          <input
+            id="attach-img"
+            type="file"
+            multiple
+            accept="image/*"
+            className="hidden"
+            onChange={onFileChange}
+          />
+        </label>
+      </form>
+
+      {images &&
+        images.map(url => {
+          <img src={url.substring(1, -1)} />
+        })}
+
+      <SwiperPicture />
+    </>
   )
 }
 
---
+++ withpet/src/components/Diary/SwiperPicture.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Swiper } from 'swiper/react'
+
+const SwiperPicture = () => {
+  return (
+    <div>SwiperPicture</div>
+  )
+}
+
+export default SwiperPicture
