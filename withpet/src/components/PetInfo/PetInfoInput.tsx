ba56106f07fabb62e4a34b153eef800520126526
sungeunnn <fronteun@naver.com>
2023-03-17 00:10:09 +0900

    [💄] petInfo Input 스타일 #35

--- withpet/src/components/PetInfo/PetInfoInput.tsx
+++ withpet/src/components/PetInfo/PetInfoInput.tsx
@@ -3,26 +3,37 @@ import 'components/App/App.css'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string
-  type: 'text' | 'date' | 'hidden'
-  value?: string
+  name?: string
+  type: 'file' | 'text' | 'date' | 'submit' | 'radio'
+  children?: string
   className?: string
-  children: string
 }
 
 const PetInfoInput = (props: InputProps) => {
-  const { id, type, value, className, children, ...rest } = props
+  const { id, name, type, children, ...rest } = props
 
   return (
-    <>
-      <label htmlFor={id}> {children} </label>
-      <input
-        id={id}
-        type={type}
-        value={value}
-        className={className}
-        {...rest}
-      />
-    </>
+    <div
+      role="inputGroup"
+      className=" flex items-center justify-center w-full"
+    >
+      <div role="flexBox" className="relative flex items-center justify-between py-2 w-full">
+        <label
+          htmlFor={id}
+          className="absolute h-12 text-xs top-6 left-3 decoration-Gray-400 font-semibold"
+        >
+          {children}
+        </label>
+        <input
+          id={id}
+          name={name}
+          type={type}
+          className="box-border w-11/12 h-12 text-right px-3 border-2 border-black "
+          {...rest}
+          required
+        />
+      </div>
+    </div>
   )
 }
 
