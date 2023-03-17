57cd2d36b182a9a1883ae3dd594452851acefb0b
chaehaeun <chae_3210@naver.com>
2023-03-17 16:14:10 +0900

    [💄] tab index 추가 #5

--- withpet/src/components/SignIn/SocialIcon.tsx
+++ withpet/src/components/SignIn/SocialIcon.tsx
@@ -7,6 +7,7 @@ type IconProps = {
 const SocialIcon: React.FC<IconProps> = ({ method }) => {
   return (
     <div
+      tabIndex={0}
       className={`w-10 h-10 bg-sprites_icon
       ${method === 'kakao' ? 'bg-[left_-11px_top_-326px]' : ''}
       ${method === 'google' ? 'bg-[left_-53px_top_-326px]' : ''}
