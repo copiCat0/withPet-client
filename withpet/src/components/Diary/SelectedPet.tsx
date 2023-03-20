51281430f61296ae2f663e131fa38e5b712902f5
Butter on Bread <qorbaxk97@gmail.com>
2023-03-20 18:01:43 +0900

    [:sparkles:] 반려동물 선택 firebase 데이터를 불러와서 렌더링 #23

--- withpet/src/components/Diary/SelectedPet.tsx
+++ withpet/src/components/Diary/SelectedPet.tsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs, DocumentData } from 'firebase/firestore'
+import { dbService } from 'firebase-config'
 
 const SelectedPet: React.FC = () => {
+  const [myPets, setMyPets] = useState<DocumentData[]>([])
 
-  const onFocusPet = (e : any) =>{
-    e.stopPropagation()
-    console.log(e.target.name)
+  useEffect(() => {
+    const getMyPet = async () => {
+      const myPetList = await getDocs(collection(dbService, 'petInfo'))
+      setMyPets(myPetList.docs.map(doc => doc.data()))
+    }
+    getMyPet()
+  }, [])
+
+  const onFocusPet = () => {
+    // e.stopPropagation()
+    // console.log(e.target.name)
   }
 
   return (
     <div>
-      <p>반려동물 선택</p>
-      <button type="button" name="petName" key="petId" onClick={onFocusPet}>
-        <figure className="w-[50px] h-[60px] bg-white flex flex-col justify-center items-center">
-          <img
-            src="https://media.discordapp.net/attachments/1080393932652945440/1083021952127615068/86a969f151bdf1ed.jpg?width=663&height=663"
-            alt="펫 네임"
-            className="w-11 h-[38px] object-cover"
-          />
-          <figcaption className="text-xs">꼬미</figcaption>
-        </figure>
-      </button>
+      <p className="text-xs mt-2">반려동물 선택</p>
+      {myPets &&
+        myPets.map(pet => (
+          <button
+            type="button"
+            name={pet.petName}
+            key={pet.petName}
+            onClick={onFocusPet}
+            className="mr-4 mt-1.5 mb-3"
+          >
+            <figure className="w-[50px] h-[60px] bg-white flex flex-col justify-center items-center">
+              <img
+                src={pet.petImg}
+                alt={pet.petName}
+                className="w-11 h-[38px] object-cover"
+              />
+              <figcaption className="text-xs">{pet.petName}</figcaption>
+            </figure>
+          </button>
+        ))}
     </div>
   )
 }
