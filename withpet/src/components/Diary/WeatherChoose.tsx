802afeba3b679f2bc6a74ca6e0bb90f62ec18e11
LeeWonJun <lwj2627@naver.com>
2023-03-20 21:19:38 +0900

    되써 되따고

--- withpet/src/components/Diary/WeatherChoose.tsx
+++ withpet/src/components/Diary/WeatherChoose.tsx
@@ -4,11 +4,51 @@ import { chooseWeather } from '../../redux/diarySlice'
 import spritesIcon from '../../assets/sprites_icon.png'
 
 const WEATHERS = [
-  { id: 'sunny', position:  '-120px -180px', selectedPosition: '-120px -140px' },
-  { id: 'rainy', position:  '-80px -180px', selectedPosition: '-80px -140px' },
-  { id: 'cloudy', position: '-40px -180px', selectedPosition: '-40px -140px' },
-  { id: 'stormy', position: '0px -180px', selectedPosition: '0px -140px' },
-  { id: 'snowy', position: '-160px -180px', selectedPosition: '-160px -140px' }
+  {
+    id: 'sunny',
+    width: '38px',
+    height: '38px',
+    selectedWidth: '40px',
+    selectedHeight: '40px',
+    position: '-125px -147px',
+    selectedPosition: '-124px -104px',
+  },
+  {
+    id: 'rainy',
+    width: '38px',
+    height: '38px',
+    selectedWidth: '40px',
+    selectedHeight: '40px',
+    position: '-84px -150px',
+    selectedPosition: '-83px -108px',
+  },
+  {
+    id: 'cloudy',
+    width: '38px',
+    height: '38px',
+    selectedWidth: '40px',
+    selectedHeight: '40px',
+    position: '-43px -151px',
+    selectedPosition: '-42px -109px',
+  },
+  {
+    id: 'stormy',
+    width: '38px',
+    height: '38px',
+    selectedWidth: '40px',
+    selectedHeight: '40px',
+    position: '-2px -148px',
+    selectedPosition: '-1px -105px',
+  },
+  {
+    id: 'snowy',
+    width: '38px',
+    height: '38px',
+    selectedWidth: '40px',
+    selectedHeight: '40px',
+    position: '-167px -146px',
+    selectedPosition: '-167px -104px',
+  },
 ]
 
 const WeatherChoose: React.FC = () => {
@@ -22,11 +62,11 @@ const WeatherChoose: React.FC = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center h-full">
-        {WEATHERS.map((weather) => (
+      <div className="flex justify-center gap-[8px] items-center w-full h-[70px] bg-Gray-100">
+        {WEATHERS.map(weather => (
           <div
             key={weather.id}
-            className={`w-1/5 h-full ${
+            className={`w-[40px] h-[40px] gap-[10px]${
               selectedWeather === weather.id
                 ? 'bg-cover bg-no-repeat bg-center cursor-pointer'
                 : 'cursor-pointer'
@@ -37,11 +77,21 @@ const WeatherChoose: React.FC = () => {
                 selectedWeather === weather.id
                   ? weather.selectedPosition
                   : weather.position
+              }`,
+              width: `${
+                selectedWeather === weather.id
+                  ? weather.selectedWidth
+                  : weather.width
+              }`,
+              height: `${
+                selectedWeather === weather.id
+                  ? weather.selectedHeight
+                  : weather.height
               }`
             }}
             onClick={() => handleWeatherSelect(weather.id)}
           />
-        ))}나와라잉 나와
+        ))}
       </div>
     </>
   )
