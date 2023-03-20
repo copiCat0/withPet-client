d0cbf145050998817b9841e58c009bd1ff6c3579
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-20 10:22:57 +0900

    [💄] Add SignUp Page #10

--- withpet/src/components/App/App.tsx
+++ withpet/src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import 'components/App/App.css'
+import SignUp from 'components/SignUp/SignUp'
 
 function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </>
   )
 }
