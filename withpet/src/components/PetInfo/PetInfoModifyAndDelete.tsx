3d0d4efb34182f907498761323fec7818996013d
sungeunnn <fronteun@naver.com>
2023-03-21 16:30:43 +0900

    [✨] 반려동물 정보 수정 기능 #37

--- withpet/src/components/PetInfo/PetInfoModifyAndDelete.tsx
+++ withpet/src/components/PetInfo/PetInfoModifyAndDelete.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import 'components/App/App.css'
-import { RootState } from 'redux/store'
 import { useSelector } from 'react-redux'
+import { RootState } from 'redux/store'
+import { deleteDoc, doc, setDoc } from 'firebase/firestore'
 import { dbService } from 'firebase-config'
-import { deleteDoc, doc } from 'firebase/firestore'
+
 
 const PetInfoModifyAndDelete: React.FC = () => {
+  const userUid = useSelector((state: RootState) => state.auth.userUid)
+  const petInfo = useSelector(
+    (petInfoState: RootState) => petInfoState.petInfo.petInfoGroup,
+  )
   const petInfoId = useSelector((state:RootState) => state.petInfo.petInfoId)
+
+  const onModifyClick = async() => {
+    await setDoc(doc(dbService, 'petInfo', petInfoId),{ ...petInfo, userUid })
+  }
+
   const onDeleteClick =  async () => {
     await deleteDoc(doc(dbService, 'petInfo', petInfoId))
   }
-
 
   return (
     <section className="flex flex-col w-full items-center">
@@ -19,6 +28,7 @@ const PetInfoModifyAndDelete: React.FC = () => {
         type="button"
         className="w-11/12 py-4 mt-6 mb-2 bg-primary-200 text-white font-bold shadow-200 cursor-pointer"
         value="수정하기"
+        onClick={onModifyClick}
       />
 
       <input
