3bbdd6b184fb9edcaf96a00d33d567f8ceb8dced
sungeunnn <fronteun@naver.com>
2023-03-22 16:52:13 +0900

    [💄] myPage MyPageEditBtn 컴포넌트 UI마크업 및 스타일 #32

---
+++ withpet/src/components/MyPage/MyPageEditBtn.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'components/App/App.css'
+
+const MyPageEditBtn:React.FC = () => {
+  return (
+    <div className="relative w-full">
+      <button 
+      type="button"
+      className="absolute w-4 h-4 m-2 bg-sprites_icon bg-[left_-1px_top_-450px] right-0 p-2"/>
+    </div>
+  )
+}
+
+export default MyPageEditBtn
